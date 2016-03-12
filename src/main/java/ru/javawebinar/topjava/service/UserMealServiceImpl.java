b3c2fe357dacf2e2fa51abb18a61db1ba304e13c
Siarhei Baltrukevich <boltic28@gmail.com>
2016-03-12 18:48:51 +0200

    spring connect

--- src/main/java/ru/javawebinar/topjava/service/UserMealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserMealServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
@@ -13,35 +14,36 @@ import java.util.stream.Collectors;
  * GKislin
  * 06.03.2015.
  */
+@Service
 public class UserMealServiceImpl implements UserMealService {
 
     @Autowired
-    private UserMealRepository repository;
+    private UserMealRepository mealRepository;
 
     @Override
     public UserMeal save(UserMeal userMeal) {
-        repository.save(userMeal);
+        mealRepository.save(userMeal);
         return userMeal;
     }
 
     @Override
     public void delete(int id) {
-        repository.delete(id);
+        mealRepository.delete(id);
     }
 
     @Override
     public UserMeal get(int id) {
-        return repository.get(id);
+        return mealRepository.get(id);
     }
 
     @Override
     public List<UserMeal> getAll(User user) {
-        return (List<UserMeal>) repository.getAllForUser(user);
+        return (List<UserMeal>) mealRepository.getAllForUser(user);
     }
 
     @Override
     public List<UserMeal> getBetweenTime(LocalDateTime start, LocalDateTime end, User user) {
-        return repository.getAllForUser(user).stream()
+        return mealRepository.getAllForUser(user).stream()
                 .filter(meal -> meal.getDateTime().isAfter(start) && meal.getDateTime().isBefore(end))
                 .sorted((m1, m2) -> m1.getDateTime().compareTo(m2.getDateTime()))
                 .collect(Collectors.toList());
--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web.meal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.service.UserMealService;
@@ -14,41 +15,42 @@ import java.util.List;
  * GKislin
  * 06.03.2015.
  */
+@Controller
 public class UserMealRestController {
     private static final Logger LOG = LoggerFactory.getLogger(UserMealRestController.class);
 
     @Autowired
-    private UserMealService service;
+    private UserMealService mealService;
 
     public List<UserMeal> getAll(User user) {
         LOG.info("getAllForMeal");
-        return service.getAll(user);
+        return mealService.getAll(user);
     }
 
     public UserMeal get(int id) {
         LOG.info("get meal " + id);
-        return service.get(id);
+        return mealService.get(id);
     }
 
     public UserMeal create(UserMeal userMeal) {
         userMeal.setId(0);
         LOG.info("create " + userMeal);
-        return service.save(userMeal);
+        return mealService.save(userMeal);
     }
 
     public void delete(int id) {
         LOG.info("delete " + id);
-        service.delete(id);
+        mealService.delete(id);
     }
 
     public void update(UserMeal userMeal, int id) {
         userMeal.setId(id);
         LOG.info("update " + userMeal);
-        service.save(userMeal);
+        mealService.save(userMeal);
     }
 
     public List<UserMeal> getBetweenTime(LocalDateTime start, LocalDateTime end) {
         LOG.info("getBetween " + start + " and " + end);
-        return service.getBetweenTime(start,end);
+        return mealService.getBetweenTime(start,end,new User());
     }
 }
